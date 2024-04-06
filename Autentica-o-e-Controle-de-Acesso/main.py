d20246fe52a89c4324e7b6bac7acb6b9b1e445c0
Otavio Murilo <132911950+otaivomuirlo@users.noreply.github.com>
2024-04-06 16:02:42 -0300

    Add files via upload

--- Autentica-o-e-Controle-de-Acesso/main.py
+++ Autentica-o-e-Controle-de-Acesso/main.py
@@ -7,37 +7,54 @@ import hashlib
 def hash_senha(senha):
     return hashlib.sha256(senha.encode()).hexdigest()
 
+def acoes():
+    while True:
+        print("===========")
+        print("1. Listar Arquivo")
+        print("1. Criar Arquivo")
+        print("1. Ler Arquivo")
+        print("1. Excluir Arquivo")
+        print("2. Executar arquivo")
+        print("3. Sair")
+        print("===========")
+        acao = input("Escolha uma opção: ")
+
+
+
 while True:
-    print("1. Entrar com uma conta existente")
-    print("2. Criar uma nova conta")
+    print("===========")
+    print("1. Entrar com um perfil existente")
+    print("2. Criar um novo perfil")
     print("3. Sair")
-    
+    print("===========")
     opcao = input("Escolha uma opção: ")
 
     if opcao == "1":
-        email = input("Digite seu email: ")
-        senha = getpass.getpass("Digite sua senha: ")
+        nome = input("Informe seu nome: ")
+        senha = getpass.getpass("Informe sua senha: ")
 
         with open("usuarios.txt", "r") as arquivo:
             usuarios = arquivo.readlines()
             usuario_registrado = False
             for linha in usuarios:
                 dados = linha.strip().split(",")
-                if email == dados[0] and hash_senha(senha) == dados[1]:
+                if nome == dados[0] and hash_senha(senha) == dados[1]:
                     usuario_registrado = True
                     print("Autenticação bem-sucedida.")
+                    print("Bem vindo", nome)
+                    acoes()
                     break
                     
             if not usuario_registrado:
-                print("Falha. Usuário ou senha inválidos.")
+                print("Usuário ou senha inválidos.")
 
     elif opcao == "2":
-        email = input("Digite o novo email: ")
-        senha = getpass.getpass("Digite sua senha: ")
+        nome = input("Informe seu nome: ")
+        senha = getpass.getpass("Informe sua senha: ")
         senha_hash = hash_senha(senha)
 
         with open("usuarios.txt", "a") as arquivo:
-            arquivo.write(f"{email},{senha_hash}\n")
+            arquivo.write(f"Nome: {nome}, Senha: {senha_hash}\n")
             print("Usuário registrado com sucesso!")
 
     elif opcao == "3":
